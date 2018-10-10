b441e748d303904bbb47034f7bc79e93841c0381
ak5352 <ak5352@nyu.edu>
2018-10-10 12:02:41 -0400

    Added Level 1 maze, floor, player, player movement, and player animation

---
+++ Sokoban/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	private float speed = 2.0f;
+	private Vector3 pos;
+	private Transform tr;
+	private Rigidbody2D myRigidbody;
+	private Animator myAnimator;
+ 
+	void Start() 
+	{
+		pos = transform.position;
+		tr = transform;
+		myRigidbody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+	}
+ 
+	void Update() {
+ 
+		if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))&& tr.position == pos)
+		{
+			myAnimator.Play("dude_right");
+			pos += Vector3.right;
+		}
+		
+		else if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && tr.position == pos) 
+		{
+			myAnimator.Play("dude_left");
+			pos += Vector3.left;
+		}
+		
+		else if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && tr.position == pos) 
+		{
+			myAnimator.Play("dude_back");
+			pos += Vector3.up;
+		}
+		
+		else if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && tr.position == pos) 
+		{
+			myAnimator.Play("dude_front");
+			pos += Vector3.down;
+		}
+     
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D info)
+	{
+		if (info.gameObject.tag.Equals("wall"))
+		{
+			SceneManager.LoadScene("Level1");
+		}
+	}
+}
